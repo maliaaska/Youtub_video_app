1e12a25197e3bb16c06065531c8abbb351e64512
Arkadiusz Milewski <asmilewski@gmail.com>
2021-01-19 11:26:26 +0100

    App.js, SearchBar.js added and inplemented the basisc insine of the class component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onINputChange = () => {};
+
+  render() {
+    return (
+      <div className="ui segment search-bar">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="Search"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
